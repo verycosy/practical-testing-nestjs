a5312b57b36cf6cdbdf5eddc170bcd57cd346d17
Jinho Jang <verycosy@kakao.com>
2023-07-10 23:00:31 +0900

    AppModule에 OrderApiModule import

--- src/api/order/order-api.module.ts
+++ src/api/order/order-api.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MailModule } from 'src/common/mail/mail.module';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { OrderStatisticsService } from './order-statistics.service';
 
 @Module({
+  imports: [MailModule],
   controllers: [OrderController],
-  providers: [OrderService],
+  providers: [OrderService, OrderStatisticsService],
 })
 export class OrderApiModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CoreModule } from './core.module';
+import { ProductApiModule } from './api/product/product-api.module';
+import { ApiSetupModule } from './api-setup.module';
+import { OrderApiModule } from './api/order/order-api.module';
 
 @Module({
-  imports: [],
+  imports: [CoreModule, ApiSetupModule, ProductApiModule, OrderApiModule],
   controllers: [],
   providers: [],
 })
