73053c99427126611b9e55c4c5627264638b4bf8
Jinho Jang <verycosy@kakao.com>
2023-07-21 23:11:51 +0900

    문자열 날짜를 LocalDate, LocalDateTime으로 변환하는 메서드 추가

--- src/util/date-time-util.ts
+++ src/util/date-time-util.ts
@@ -1,6 +1,18 @@
-import { LocalDate, LocalDateTime, convert, nativeJs } from '@js-joda/core';
+import {
+  DateTimeFormatter,
+  LocalDate,
+  LocalDateTime,
+  convert,
+  nativeJs,
+} from '@js-joda/core';
 
 export class DateTimeUtil {
+  private static LOCAL_DATE_FORMATTER =
+    DateTimeFormatter.ofPattern('yyyy-MM-dd');
+  private static LOCAL_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(
+    'yyyy-MM-dd HH:mm:ss',
+  );
+
   static toDate(localDate: LocalDate | LocalDateTime): Date {
     return convert(localDate).toDate();
   }
@@ -11,5 +23,13 @@ export class DateTimeUtil {
 
   static toLocalDateTime(date: Date): LocalDateTime {
     return LocalDateTime.from(nativeJs(date));
+  }
+
+  static toLocalDateBy(str: string) {
+    return LocalDate.parse(str, DateTimeUtil.LOCAL_DATE_FORMATTER);
+  }
+
+  static toLocalDateTimeBy(str: string) {
+    return LocalDateTime.parse(str, DateTimeUtil.LOCAL_DATE_TIME_FORMATTER);
   }
 }
---
+++ test/util/date-time-util.spec.ts
@@ -0,0 +1,30 @@
+import { LocalDate, LocalDateTime } from '@js-joda/core';
+import { DateTimeUtil } from 'src/util/date-time-util';
+
+describe('DateTimeUtil', () => {
+  it('yyyy-MM-dd 포맷의 문자열을 LocalDate 객체로 변환한다', () => {
+    // given
+    const str = '2023-07-21';
+    const localDate = LocalDate.of(2023, 7, 21);
+
+    // when
+    const result = DateTimeUtil.toLocalDateBy(str);
+
+    // then
+    expect(result).toBeInstanceOf(LocalDate);
+    expect(result.isEqual(localDate)).toBe(true);
+  });
+
+  it('yyyy-MM-dd HH:mm:ss 포맷의 문자열을 LocalDateTime 객체로 변환한다', () => {
+    // given
+    const str = '2023-07-21 23:05:30';
+    const localDateTime = LocalDateTime.of(2023, 7, 21, 23, 5, 30);
+
+    // when
+    const result = DateTimeUtil.toLocalDateTimeBy(str);
+
+    // then
+    expect(result).toBeInstanceOf(LocalDateTime);
+    expect(result.isEqual(localDateTime)).toBe(true);
+  });
+});
